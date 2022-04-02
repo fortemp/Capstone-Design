2880cd78f0ace39b685f7be4abc4a3973a6eb408
ktj9701 <ktj9701@naver.com>
2022-04-02 16:16:32 +0900

    채팅화면

--- client/src/Pages/LandingPage/Sections/ChatSection.js
+++ client/src/Pages/LandingPage/Sections/ChatSection.js
@@ -1,14 +1,32 @@
-import React,{useEffect} from 'react'
+import React,{useEffect,useState} from 'react'
 import Box from '@material-ui/core/Box'
-import { TextField } from '@mui/material'
 import "./ChatSection.css"
+
+
 
 
 function ChatSection(props) {
 
-  return (
-    <Box style={props.style} bgcolor={"#888888"} color={"#222222"}  >
-         <div id="Chatting">채팅창</div>
+  const [chattext, setText2]=useState('');
+  const [text, setText] = useState('');
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+  const submitChat = ()=>{ // 입력포트값으로 정보 전달
+   setText('');
+   setText2(text);
+  };
+
+  const add='<span>'+chattext+'</span>';
+
+  return (                                                               //메시지 입력이 연속적으로 출력되지 않고 하나의 메시지만 출력됨( 고쳐야함 )
+    <Box style={props.style} bgcolor={"#888888"} color={"#222222"}  >               
+         <div id="Chatting" dangerouslySetInnerHTML={{__html:add}}></div>    
+         <div className='Submit'>                                
+           <input className='Msg' type='text' value={text} name="txtChat" onChange={onChange} /> 
+            <button className='SubmitBtn'type='button' name="txtChat" onClick={submitChat}>↑</button>
+         </div>
     </Box>
   )
 }
