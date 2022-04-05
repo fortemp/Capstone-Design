e8607b33edaa0efa7b9e4996cb35de16f441bfb8
ktj9701 <ktj9701@naver.com>
2022-04-05 15:09:43 +0900

    게시글 페이지

--- client/src/Pages/CommunityPage.js
+++ client/src/Pages/CommunityPage.js
@@ -1,15 +1,58 @@
-import{Link} from "react-router-dom";
-import React from 'react'
+import React, { useEffect } from 'react'
+import ChatSection from './LandingPage/Sections/ChatSection'
+import BoardSection from './LandingPage/Sections/BoardSection'
+import RoomSection from './LandingPage/Sections/RoomsSection'
+import RankingSection from './LandingPage/Sections/RankingSection'
+import Grid from '@material-ui/core/Grid';
+import { SocketContext, Roomsocket, Publicsocket } from '../api/socket'
+function CommunityPage() {
+    const RankingStyle = { height: '740px' }
+    const roomStyle = { height: '200px' }
+    const chatStyle = { height: '532px'}
+    const gameStyle = { height: '740px' }
 
-function CommunityPage() { //임시로 null
+  
 
-  return(
-    <div>
-      <Link to='/Posting'> 
-        <button> 글작성 </button>
-      </Link>
-    </div>
-  ); 
+    return (
+        <SocketContext.Provider value={{ room: Roomsocket, public: Publicsocket }}>
+            <Grid container spacing={1}>
+                <Grid item xs={12} md={3}>
+                    <RankingSection style={RankingStyle} />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <BoardSection style={gameStyle} />
+                </Grid>
+
+                <Grid item xs={12} md={3}>
+
+                    <Grid
+                        container
+                        spacing={1}
+                        direction="column" padding={"0 0 0 0"}>
+
+                        <Grid item>
+                            <RoomSection style={roomStyle} />
+                        </Grid>
+
+                        <Grid item>
+                            <ChatSection style={chatStyle} />
+                        </Grid>
+
+                    </Grid>
+
+                </Grid>
+            </Grid>
+
+
+
+        </SocketContext.Provider>
+    )
 }
 
 export default CommunityPage
+
+
+
+
+
+
