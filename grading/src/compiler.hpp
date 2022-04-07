78dd5b19b105ae1c1462015cd9e2f1760f9c2780
fortemp <gnl5672@naver.com>
2022-04-07 17:21:17 +0900

    오류 타입 enum class 생성 반환값 수정

--- grading/src/compiler.hpp
+++ grading/src/compiler.hpp
@@ -3,6 +3,13 @@
 #include <unistd.h>
 
 namespace Grading{
+    enum class ErrorType{
+        None,
+        NoFileError,
+        CompileError,
+        RuntimeError
+    }
+
     class Executer{
         protected:
             std::string output_;   //실행파일 경로
@@ -13,25 +20,31 @@ namespace Grading{
             Executer(const std::string& output, const std::string& result, const std::string& log)
             :output_(output), result_(result), log_(log){};
 
-            std::string execute(std::string path, std::string file_name){
+            std::pair<ErrorType, std::string> execute(std::string path, std::string file_name){
                 /*
                 *   실제로 실행하고 출력값이 위치한 경로를 리턴한다.
                 *   Args:
                 *       path:       data 값이 있는 곳 
                 *       file_name:  data 값의 이름 (확장자 제외)
                 *   Returns:
-                *       std::string or "" : 실행이 완료되면 결과값이 있는 파일의 경로를 전송, 실행중 오류가 날 경우 빈 문자열
+                *       std::pair<ErrorType, std::string>
+                *       ErrorPath   : ErrorType과 path를 동시에 전달
+                *       std::string : 실행이 완료되면 결과값이 있는 파일의 경로를 전송, 실행중 오류가 날 경우 로그의 경로
                 */
 
                 const std::string data = build_path(path, file_name + ".in");
+
+                //무한 루프 감지
+
                 int reuslt = execute("./%s < %s > %s 2> %s", 
                                       output_.c_str(), data.c_str(), result_.c_str(), log_.c_str());
 
-                if(result){                 //런타임 오류가 난 경우
-                    return std::string("");
+                //런타임 오류가 난 경우
+                if(result){
+                    return std::make_pair(ErrorType::RuntimeError, log_);
                 }
 
-				return result_; 
+				return std::make_pair(ErrorType::None, result_);
             }
     };
 
@@ -54,14 +67,15 @@ namespace Grading{
             CppCompiler(const std::string& option)
             :Compiler(option, "cpp", "out"){};
 
-            virtual std::unique_ptr<Executer> compile(std::string path, std::string file_name) noexcept {
+            virtual std::pair<ErrorType, std::unique_ptr<Executer>> compile(std::string path, std::string file_name) noexcept {
                 /*
                 *   compile 해서 실행 파일을 가지고 있는 Executer를 리턴한다.
                 *   Args:
                 *       path:       파일이 위치한 경로
                 *       file_name:  파일의 이름 (확장자 제외)
                 *   Returns:
-                *       unique_ptr
+                *       std::pair<ErrorType, std::unique_ptr<Executer>>
+                *       ErrorType: 에러 타입
                 *       Executer or nullptr: 파일이 없거나 컴파일 오류가 난경우 nullptr을 반환 
                 */
                 const std::string code    = build_path(path, file_name + "." + code_extension_); 
@@ -69,19 +83,20 @@ namespace Grading{
                 const std::string result  = build_path(path, file_naem + ".result");                  //결과 파일 위치
                 const std::string log     = build_path(path, file_name + ".log");                     //컴파일, 런타임 오류 로거
 
-                //파일 있는지 확인
+                //파일이 없는 경우
                 if(access(code.c_str(), F_OK)){
-                    return nullptr;    //파일이 없다면 NULL
+                    return std::make_pair(ErrorType::NoFileError, nullptr);
                 }
 
                 int result = execute("g++ %s -o %s %s 2> %s", 
                         code.c_str(), output.c_str(), option_.c_str(), log.c_str()); 
 
-                if(result){             //컴파일 오류가 난 경우
-                    return nullptr;
+                //컴파일 오류가 난 경우
+                if(result){             
+                    return std::make_pair(ErrorType::CompileError, nullptr);
                 }
 
-                return std::make_unique<Executer>(output, result, log);
+                return std::make_pair(ErrorType::None, std::make_unique<Executer>(output, result, log));
             }
 
             virtual ~CppCompiler(){};
