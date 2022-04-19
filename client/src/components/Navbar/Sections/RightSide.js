2039c16c3c5751855132243653ab535a782880d7
ktj9701 <ktj9701@naver.com>
2022-04-19 14:33:53 +0900

    오른쪽 버튼 로고 / css 좀 더 만져야함

--- client/src/components/Navbar/Sections/RightSide.js
+++ client/src/components/Navbar/Sections/RightSide.js
@@ -1,88 +1,127 @@
-import React,{useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Button from '@mui/material/Button';
-import {useDispatch} from 'react-redux'
-import {logoutUser} from '../../../actions/index'
+import { useDispatch } from 'react-redux'
+import { logoutUser } from '../../../actions/index'
 import LogoutIcon from '@mui/icons-material/Logout';
 import LoginIcon from '@mui/icons-material/Login';
 import BorderColorIcon from '@mui/icons-material/BorderColor';
 import GiteIcon from '@mui/icons-material/Gite';
-
+import "./RightSide.css";
 function RightSide() {
 
     const dispatch = useDispatch();
-    const isAuth = useSelector(state=>state.authReducer.authData.auth);
-    const logoutHandler = ()=>{
+    const isAuth = useSelector(state => state.authReducer.authData.auth);// 사용자 정보 객체
+    const logoutHandler = () => {
         dispatch(logoutUser())
-        .then(res=>{
-            if(res.payload.success){
-                window.location.reload();
-            }else{
-                alert("실패했습니다."+res.payload.message);
-            }
-        })
+            .then(res => {
+                if (res.payload.success) {
+                    window.location.reload();
+                } else {
+                    alert("실패했습니다." + res.payload.message);
+                }
+            })
     }
 
-    const fontStyle = { 
-        fontSize:'15px'
+    const fontStyle = {
+        fontSize: '15px'
     }
 
 
-    if(!isAuth)
-    {
+
+    const [login, setlogin] = useState("false");
+    useEffect(() => {          //로그인 확인 코드
+        console.log(isAuth);
+        if (isAuth == true)
+            setlogin("true");
+
+
+    })
+
+    if (!isAuth) {
         return (
-            <div id="rightside">
 
-                <Button color="inherit">
+            <div id="rightside">
+                <div className='Info'>
+                    {login == 'false' ?
+                        <textarea className='InfoMsg' resize='none' placeholder='로그인 후 이용해 주세요'>
+                        </textarea>
+                        :
+                        <textarea className='InfoMsg' resize='none' placeholder='로그인 성공'>
+                        </textarea>
+                    }
+                </div>
+                <div className='Info2'>
+                    <Button color="inherit">
+                        <a href="/community">
+                            <BorderColorIcon
+                                sx={{
+                                    color: "#333333"
+                                }} />
+                            <span style={fontStyle}>COMMUNITY</span>
+                        </a>
+                    </Button>
+
+                    <Button color="inherit">
+                        <a href="/shop">
+                            <BorderColorIcon
+                                sx={{
+                                    color: "#333333"
+                                }} />
+                            <span style={fontStyle}>SHOP</span>
+                        </a>
+
+                    </Button>
+                    <Button color="inherit">
                         <a href="/signup">
-                            <BorderColorIcon 
-                            sx={{
-                                color:"#333333"
-                            }}/>
+                            <BorderColorIcon
+                                sx={{
+                                    color: "#333333"
+                                }} />
                             <span style={fontStyle}>register</span>
                         </a>
-                </Button>
-  
-                <Button color="inherit">
+                    </Button>
+
+                    <Button color="inherit">
                         <a href="/login">
                             <LoginIcon
-                            sx={{
-                                color:"#333333"
-                            }}/>
+                                sx={{
+                                    color: "#333333"
+                                }} />
                             <span style={fontStyle}>login</span>
                         </a>
-                </Button>
-
+                    </Button>
+                </div>
             </div>
         )
-    }else{
+    } else {
         return (
             <div id="rightside">
-    
+
                 <Button color="inherit">
-                        <a href="/host">
-                            <GiteIcon
+                    <a href="/host">
+                        <GiteIcon
                             sx={{
-                                color:"#333333"
-                            }}/>
-                            <span style={fontStyle}>host</span>
-                        </a>
+                                color: "#333333"
+                            }} />
+                        <span style={fontStyle}>host</span>
+                    </a>
                 </Button>
 
                 <Button color="inherit">
-                        <a onClick={logoutHandler}>
-                            <LogoutIcon
+                    <a onClick={logoutHandler}>
+                        <LogoutIcon
                             sx={{
-                                color:"#333333"
-                            }}/>
-                            <span style={fontStyle}>logout</span>
-                        </a>
+                                color: "#333333"
+                            }} />
+                        <span style={fontStyle}>logout</span>
+                    </a>
                 </Button>
-    
+
             </div>
         )
     }
-        
+
 }
 
 export default RightSide
