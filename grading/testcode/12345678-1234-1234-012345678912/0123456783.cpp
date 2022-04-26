fa9000babf5742d37217c0b103f708d0f9106462
fortemp <gnl5672@naver.com>
2022-04-26 16:15:07 +0900

    무한루프 테스트파일 추가

---
+++ grading/testcode/12345678-1234-1234-012345678912/0123456783.cpp
@@ -0,0 +1,12 @@
+//normal
+#include <iostream>
+
+int main(){
+    int a, b;
+    std::cin >> a >> b;
+    while(true){
+        int c = a + b;
+    } 
+    std::cout << a+b << std::endl;
+    return 0;
+}
