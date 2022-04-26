a3e682e66111850cf0dc853862c478edd945b32f
fortemp <gnl5672@naver.com>
2022-04-26 16:11:56 +0900

    컴파일러 코드 구조 변경

--- grading/src/compiler.hpp
+++ grading/src/compiler.hpp
@@ -1,104 +1,160 @@
 #pragma once
 #include "util.hpp"
+#include "exception.hpp"
 #include <unistd.h>
+#include <memory>
+#include <thread>
+#include <chrono>
+#include <future>
+#include <condition_variable>
+#include <fcntl.h>
+#include <sys/wait.h>
 
 namespace Grading{
-    enum class ErrorType{
-        None,
-        NoFileError,
-        CompileError,
-        RuntimeError
-    }
-
     class Executer{
         protected:
-            std::string output_;   //실행파일 경로
-            std::string result_;   //결과파일 경로
-            std::string log_;      //로그 파일 경로
+            std::string target_path_;           //실행 대상 경로
+            bool done_;                         //프로그램이 잘 실행되었을 경우 True
+            bool time_out_;                     //프로그램이 시간초과가 났을 경우 True
 
         public:
-            Executer(const std::string& output, const std::string& result, const std::string& log)
-            :output_(output), result_(result), log_(log){};
+            Executer(const std::string& target_path)
+                     :target_path_(target_path){
+                        done_ = false;
+                        time_out_ = false;
+                     };
 
-            std::pair<ErrorType, std::string> execute(std::string path, std::string file_name){
+            template<typename T>
+            int run(const std::string& data_path, const std::string& result_path, const std::string& log_path, T time_limit){
                 /*
-                *   실제로 실행하고 출력값이 위치한 경로를 리턴한다.
-                *   Args:
-                *       path:       data 값이 있는 곳 
-                *       file_name:  data 값의 이름 (확장자 제외)
-                *   Returns:
-                *       std::pair<ErrorType, std::string>
-                *       ErrorPath   : ErrorType과 path를 동시에 전달
-                *       std::string : 실행이 완료되면 결과값이 있는 파일의 경로를 전송, 실행중 오류가 날 경우 로그의 경로
+                *   target_path를 실행하고 그 결과를 result_path와 log_path에 기록한다.
+                *   args:
+                *       data_path:   실행을 위한 데이터 경로
+                *       result_path: 실행 결과가 기록될 파일의 경로
+                *       log_path:    실행 도중 생기는 warning이나 error를 기록할 로그의 경로
+                *       time_limit:  시간 제한
+                *   return:
+                *       정상 실행이 종료된 경우 0을 반환한다.
+                *   
+                *   except:
+                *       RuntimeExecption: 실행 도중 오류가 난 경우 반환
                 */
 
-                const std::string data = build_path(path, file_name + ".in");
+                done_ = false;
+                time_out_ = false;
 
-                //무한 루프 감지
+                int pid = fork();
+                int input_file_descriptor = open(data_path.c_str(), O_RDONLY);
+                int output_file_descriptor = open(result_path.c_str(), O_CREAT | O_RDWR, 0666);
+                int log_file_descriptor = open(log_path.c_str(), O_WRONLY);
 
-                int reuslt = execute("./%s < %s > %s 2> %s", 
-                                      output_.c_str(), data.c_str(), result_.c_str(), log_.c_str());
+                if(input_file_descriptor < 0) throw CantOpenFileException(data_path);
+                if(output_file_descriptor < 0) throw CantOpenFileException(result_path);
+                if(log_file_descriptor < 0) throw CantOpenFileException(log_path);
 
-                //런타임 오류가 난 경우
-                if(result){
-                    return std::make_pair(ErrorType::RuntimeError, log_);
+                if(pid < 0){
+                    //fork가 정상적으로 시행이 안되었을 때
                 }
+                else if(pid == 0){
+                    //자식 프로세스 내부
+                    
+                    //입력 파이프라인
+                    dup2(input_file_descriptor, STDIN_FILENO);
+                    dup2(output_file_descriptor, STDOUT_FILENO); //STDOUT이 잘 해결되지 않고 있음
+                    dup2(log_file_descriptor, STDERR_FILENO);
 
-				return std::make_pair(ErrorType::None, result_);
+                    //파일 실행
+                    execl(target_path_.c_str(), target_path_.c_str(), NULL);
+                }
+                else{
+                    //부모 프로세스 내부
+                    
+                    //자식 프로세스 대기
+                    int statloc = 0;
+                    auto wait_child = std::async(std::launch::async, 
+                    [&](int fpid, int* fstatloc, int foption){
+                        waitpid(fpid, fstatloc, foption);
+                        done_ = true;
+                        return;
+                    }, 
+                    pid, &statloc, 0/*option*/);
+                    
+
+                    std::future_status status = wait_child.wait_for(time_limit);
+
+                    if(status == std::future_status::timeout){
+                        close(input_file_descriptor);
+                        close(output_file_descriptor);
+                        close(log_file_descriptor);
+                        execute("kill -9 %d", pid);
+                        return 1;
+                    }
+
+                    return 0;
+
+                }
+                return -1;
             }
     };
 
     class Compiler{
         protected:
-            std::string option_;              //컴파일 옵션
-            std::string code_extension_;      //코드 확장자 ex) cpp, py
-            std::string output_extension_;    //실행 파일 확장자 ex) out
+            std::string format_;                 //컴파일 명령어
+            std::string option_;                 //컴파일러 옵션
+
+            std::string target_path_;            //컴파일 대상 경로
+            std::string result_path_;            //컴파일 결과물 경로
+            std::string log_path_;               //컴파일 로그 파일 경로
 
         public:
-            Compiler(const std::string& option, const std::string& code_extension, const std::string& output_extension)
-            :option_(option), code_extension_(code_extension), output_extension_(output_extension){};
+            Compiler(const std::string& format,
+                     const std::string& option, 
+                     const std::string& target_path,
+                     const std::string& result_path,
+                     const std::string& log_path)
+                     : format_(format),
+                       option_(option),
+                       target_path_(target_path),
+                       result_path_(result_path),
+                       log_path_(log_path){}
 
-            virtual std::unique_ptr<Executer> compile(std::string path, std::string file_name) = 0;
+            virtual std::unique_ptr<Executer> compile() const = 0;
             virtual ~Compiler(){};
     };
 
     class CppCompiler: public Compiler{
         public:
-            CppCompiler(const std::string& option)
-            :Compiler(option, "cpp", "out"){};
+            CppCompiler(const std::string& option, 
+                        const std::string& target_path,
+                        const std::string& result_path,
+                        const std::string& log_path)
+                        : Compiler("g++ %s -o %s %s 2> %s",
+                            option, target_path, result_path, log_path){};
 
-            virtual std::pair<ErrorType, std::unique_ptr<Executer>> compile(std::string path, std::string file_name) noexcept {
+            virtual std::unique_ptr<Executer> compile() const override{
                 /*
-                *   compile 해서 실행 파일을 가지고 있는 Executer를 리턴한다.
-                *   Args:
-                *       path:       파일이 위치한 경로
-                *       file_name:  파일의 이름 (확장자 제외)
-                *   Returns:
-                *       std::pair<ErrorType, std::unique_ptr<Executer>>
-                *       ErrorType: 에러 타입
-                *       Executer or nullptr: 파일이 없거나 컴파일 오류가 난경우 nullptr을 반환 
+                *   compile을 실행하고 무사히 실행한 경우 Executer의 unique_ptr을 반환한다.
+                *   
+                *   return:
+                *       unique_ptr<Executer> 실행할 수 있는 파일의 경로를 가지고 있는 Executer 포인터
+                *
+                *   except:
+                *       NoFileException: 컴파일 할 수 있는 파일을 확인하지 못했을 경우 
+                *       CompileException: 컴파일 도중 out파일이 제대로 생성되지 못한 경우 반환
                 */
-                const std::string code    = build_path(path, file_name + "." + code_extension_); 
-                const std::string output  = build_path(path, file_name + "." + output_extension_);
-                const std::string result  = build_path(path, file_naem + ".result");                  //결과 파일 위치
-                const std::string log     = build_path(path, file_name + ".log");                     //컴파일, 런타임 오류 로거
 
-                //파일이 없는 경우
-                if(access(code.c_str(), F_OK)){
-                    return std::make_pair(ErrorType::NoFileError, nullptr);
-                }
+                // 파일이 존재하지 않는 경우 예외 발생
+                if(access(target_path_.c_str(), F_OK)) throw NoFileException();
 
-                int result = execute("g++ %s -o %s %s 2> %s", 
-                        code.c_str(), output.c_str(), option_.c_str(), log.c_str()); 
+                int result = execute(format_, 
+                target_path_.c_str(), result_path_.c_str(), option_.c_str(), log_path_.c_str());
 
-                //컴파일 오류가 난 경우
-                if(result){             
-                    return std::make_pair(ErrorType::CompileError, nullptr);
-                }
+                // 정상적으로 컴파일 되지 않은 경우
+                if(result != 0) throw CompileException();
 
-                return std::make_pair(ErrorType::None, std::make_unique<Executer>(output, result, log));
+                return std::make_unique<Executer>(result_path_);
             }
-
+            
             virtual ~CppCompiler(){};
     };
 }
