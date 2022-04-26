38de433d7881f5eb65208df35987cdefd2dbe254
fortemp <gnl5672@naver.com>
2022-04-26 16:17:16 +0900

    파일 존재하지 않음 예외 추가

--- grading/src/exception.hpp
+++ grading/src/exception.hpp
@@ -2,6 +2,21 @@
 #include <exception>
 
 namespace Grading{
+    class CantOpenFileException: public std::exception{
+        public:
+            CantOpenFileException(const std::string& file_name)
+            :std::exception(), file_name_(file_name){
+            }
+
+            const char* what() const noexcept override{
+                std::string message = "exception: Cant Open \"" + file_name_ + "\" file"; 
+                std::cout << message << std::endl;
+                return message.c_str();
+            }
+        private:
+            const std::string file_name_;
+    };
+
     class NoFileException: public std::exception{
         public:
             const char* what() const noexcept override{
