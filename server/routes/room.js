528f52a33b3c9960017aeffc843b389f32fc3184
kimmingyu <kimmin36944@gmail.com>
2022-04-30 19:58:52 +0900

    추가:room라우터 createRoom 구현

--- server/routes/room.js
+++ server/routes/room.js
@@ -1,21 +1,44 @@
 const express = require('express');
 const router = express.Router();
-const {Room} = require('../models/Room')
-//유저정보 가져오는 api
-router.get('/getrooms',(req,res)=>{
-    Room.find().
-    exec()
-    .then(rooms=>{
-        rooms.forEach((room,index)=>{
-            if(room.password){
-                room.password = "yes"//패스워드는 직접 안넘겨준다.
-            }
-        })
-        return res.status(200).json({success:true,rooms})
-    })
-    .catch(err=>{
-        return res.status(400).json({success:false,err})
-    })
-})
+const Room = require('../models/Room')
 
+//방들 가져오기
+router.get('/getrooms',async (req,res)=>{
+    let rooms = await Room.findAll({})
+    rooms.forEach((room,index)=>{
+        if(room.ispass === 1){
+            room.password = "yes";
+        }
+    })
+    res.status(200).json({success:true,rooms:rooms})
+})
+//방 생성하기
+router.post('/createroom',async (req,res)=>{
+    console.log(req.body)
+    let passwordInput = req.body.password;
+    let ispass = 1;
+    if(!req.body.password){
+        passwordInput = null;
+    }
+    if(passwordInput===null)
+        ispass = 0;
+
+    let object = {
+        title: req.body.title,
+        password: passwordInput,
+        ispass:ispass,
+        max_people:req.body.maxpeople,
+        language:req.body.language,
+        user_id:req.body.user_id,
+        rounds:req.body.rounds
+    }
+    try{
+        await Room.create(object);
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({success:false,message:err})
+    }
+    return res.status(200).json({success:true})
+   
+})
 module.exports = router;
