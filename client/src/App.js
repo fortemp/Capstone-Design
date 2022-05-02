861fb98fb8d70cb4ef6498115ced8a1323ac7bec
ktj9701 <ktj9701@naver.com>
2022-05-02 23:04:32 +0900

    App

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,6 @@ function App() {
           <Route exact path="/host" element={ withAuth(HostRoomPage,true) } />
           <Route exact path="/shop" element={ withAuth(ShopPage,true) } />
           <Route exact path="/community" element={ withAuth(CommunityPage,true) } />
-          <Route  path={"/game/:room_id"} element={ withAuth(GamePage,true) } />
           <Route exact path="/posting" element={ withAuth(Posting,true) } /> 
           <Route  path={"/PostPage/:post_id"} element={ withAuth(PostPage,true)} />
         </Routes>
