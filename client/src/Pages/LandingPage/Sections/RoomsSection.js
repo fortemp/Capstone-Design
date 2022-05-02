17ba0b1dd0442449007e7be332a2379f24823915
ktj9701 <ktj9701@naver.com>
2022-05-02 17:48:28 +0900

    roomsection

--- client/src/Pages/LandingPage/Sections/RoomsSection.js
+++ client/src/Pages/LandingPage/Sections/RoomsSection.js
@@ -37,7 +37,7 @@ function RoomsSection(props) {
       password = prompt("비밀번호를 입력해주세요");
       if(password===null || password===undefined || password==="")
         return;
-    }
+    } 
     setRequesting(true);//소켓요청
     roomSocket.emit('joinRoom',room_id,title,password);
     window.location.replace(`/game/${room_id}`);
