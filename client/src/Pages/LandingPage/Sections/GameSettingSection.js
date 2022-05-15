736386ce79fbac1696dd19903eb542b8b3ab0a50
ktj9701 <ktj9701@naver.com>
2022-05-15 17:14:13 +0900

    게임설정섹션(방정보를 세션에 저장하도록 변경 , 방에 나가면 props 초기화)

--- client/src/Pages/LandingPage/Sections/GameSettingSection.js
+++ client/src/Pages/LandingPage/Sections/GameSettingSection.js
@@ -11,17 +11,17 @@ function GameSettingSection(props) {
 
   const [round, setround] = useState([]);
   useEffect(() => {
-    Axios.get('/api/room/getround', {              //일단 이렇게 하면 유저 정보 가져오긴 함
+    Axios.get('/api/room/getroominfo', {              //일단 이렇게 하면 방정보 가져오긴 함
       params: {
-        'title': 'ㄴㅇㅇ',           //방 제목으로 데이터베이스 접근하는데 어떻게 방제목을 가져와야할지 모르겠음
+        'title':sessionStorage.getItem('title'),           //방 제목 가져옴
       }
     }).then((response) => {
       setround(response.data);
     })
   }, []);
 
-  const timer = setInterval(()=>{
-    clearInterval(timer);
+  const timer = setInterval(function (){
+    clearInterval(timer)
     if(seconds<59)
     setSeconds(seconds+1);
     else{
@@ -33,20 +33,21 @@ function GameSettingSection(props) {
     <Box style={props.style} bgcolor={'#eeeeee'} p={2}>
       <div style={props.style}>
       <div>
-      <button onClick={function (e) { e.preventDefault(); props.onChangeMode('normal'); }.bind(this)}>방에 나갔을 때 버튼 </button>
+      <button onClick={(e)=> { e.preventDefault(); props.onChangeMode('normal'); props.onChangeReady('false'); props.onChangeStart('false');}}>방에 나갔을 때 버튼 </button>
       {props.start == "false" ?
         <>
-          {props.ready == "false" ?
-            <button className="raady_btn" onClick={function (e) { e.preventDefault(); props.onChangeReady('true'); }.bind(this)}>레디 버튼 </button>
-            :
-            <button className="raady_btn2" onClick={function (e) { e.preventDefault(); props.onChangeReady('false'); }.bind(this)}>레디 풀기 버튼 </button>
-          }
+              {props.ready == "false" ?
+                <button className="raady_btn" onClick={function (e) { e.preventDefault(); props.onChangeReady('true'); }.bind(this)}>레디 버튼 </button>
+                :
+                <button className="raady_btn2" onClick={function (e) { e.preventDefault(); props.onChangeReady('false'); }.bind(this)}>레디 풀기 버튼 </button>}
 
-          <button className="start_btn" onClick={ (e)=> { e.preventDefault(); props.onChangeStart('true'); timer()}}>시작 버튼</button>
-          <h3>평균 ELO: 2000</h3></>  //방장만 보이도록 해야함
+              <button className="start_btn" onClick={(e) => { e.preventDefault(); props.onChangeStart('true'); timer(); } }>시작 버튼</button>
+              <h3>평균 ELO: 2000</h3> 
+              <h3>인원수: {round.people} / {round.max_people} </h3>      
+              </> // 인원수 방 정보 가져와야함
         :
         <>
-          <button className="start_btn2" onClick={function (e) { e.preventDefault(); props.onChangeStart('false'); }.bind(this)}>시작 풀기 버튼</button>  
+          <button className="start_btn2" onClick={ (e)=> { e.preventDefault(); props.onChangeStart('false');}}>시작 풀기 버튼</button>  
           <div style={{height:'30px'}}>
           <h3 style={{float:"left"}}>라운드 수 1 / {round.rounds} &nbsp;</h3>  
           <h3 style={{float:"left"}}>time: {minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h3>
