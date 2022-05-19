62f51b8e8332d8142013223eb35769d287d6b62b
kimmingyu <kimmin36944@gmail.com>
2022-05-19 11:52:17 +0900

    추가: 게임섹션 구현; player컴포넌트, 에디터추가

--- client/src/Pages/LandingPage/Sections/GameSection.js
+++ client/src/Pages/LandingPage/Sections/GameSection.js
@@ -1,198 +1,94 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState,useContext} from 'react'
+import {SocketContext} from '../../../api/socket'
 import Box from '@material-ui/core/Box'
 import './GameSection.css'
-import {useDispatch,useSelector} from 'react-redux';
+import Player from '../../../components/player/Player';
+import CodeSection from '../../../components/codeSection/CodeSection';
 
 function GameSection(props) {
 
-  const playername= useSelector(state=>state.authReducer.authData.user.name);
-  const banish= useSelector(state=>state.authReducer.authData.user.name)+'  강퇴';
-  
+  const socket = useContext(SocketContext);
+  const roomSocket = socket.room;
   useEffect(() => {
     props.onChangeReady('false');
-    setcodeInput(" ");
   }, []);
 
+  useEffect(()=>{
+    var myObjectGlobal;
+    roomSocket.off('roomInfoG').on('roomInfoG',(data)=>{
+      setRoomTitle(data.title)
+      if(data.language==='c'||data.language==='cpp')//c랑 cpp는 문법 하이라이트가 c_cpp로 같다 따로 구분해줄 필요가 있음.
+        setLanguage('c_cpp')
+      else
+        setLanguage(data.language);
+      setRoomId(data.room_id);
+      
+    })
+    roomSocket.off('roomPlayersG').on('roomPlayersG',(data)=>{
+      let temp = [];
+      let index = 0;
+      for(let i = 0; i<data.length;i++){
+        if(typeof(data[i])==="object"){//객체만따로뺌(나머지는 socketid임 필요X)
+          temp[index] = data[i]
+          index++;
+        }
+      }
+      setPlayers(temp);
+      if(myObjectGlobal){
+        for(let i=0;i<temp.length;i++){
+          if(temp[i].socketId===myObjectGlobal.socketId && temp[i].host===true){
+            setMyobject(temp[i]);
+          }
+        }
+      }
+    })
+    roomSocket.off('roomMeG').on('roomMeG',(data)=>{
+      setMyobject(data.me)
+      myObjectGlobal = data.me;
+    })
+    roomSocket.off('gameStarting').on('gameStarting',(data)=>{
+      props.onChangeStart('true');
+    })
+    roomSocket.off('startGameErr').on('startGameErr',(data)=>{
+      alert(data.message)
+    })
+  },[socket])
 
-  const [codeInput, setcodeInput] = useState("");
-
-
-
-  const codeInputTabHandler = (event) => {
-    if (event.key === "Tab") {
-      event.preventDefault();
-      setcodeInput(codeInput + '\t');
-    }
-  }
-  const codeInputHandler = (event) => {
-    setcodeInput(event.currentTarget.value);
-  }
+  const [roomId, setRoomId] = useState('');
+  const [code, setCode] = useState("");
+  const [roomTitle,setRoomTitle] = useState("불러오는중")
+  const [Myobject, setMyobject] = useState({});//나의정보
+  const [players, setPlayers] = useState([]);//플레이어들의 정보
+  const [isRequesting,setIsRequesting] = useState(false);//현재 요청중인가
+  const [language,setLanguage] = useState("c_cpp");
 
   const submit_text = () => {
-    alert("제출성공\n코드: " + codeInput.replace(/(\s*)/g, ""));
+    roomSocket.emit('sendCode',code)
+  }
+
+  const onChange=(e)=>{
+    setCode(e);
+  }
+
+  const Eject = (host,whoSocket)=>{
+    if(window.confirm('강퇴하시겠습니까?'))
+      roomSocket.emit("Eject",{host,whoSocket,roomId});
   }
 
   return (
     <Box className="Over2" style={props.style} bgcolor={"#888888"} color={"#222222"} p={2}>
-      {props.start == "false" ?                           //랜딩페이지에서 받아오는 usestate 시작, 게임세팅섹션에서도 이용해야해서 랜딩페이지로부터 호출
+      {props.start == "false" ?         //랜딩페이지에서 받아오는 usestate 시작, 게임세팅섹션에서도 이용해야해서 랜딩페이지로부터 호출
         <>
-          <div className='gameroom_title'>
-            <h3>방제목</h3>
-          </div>
+            <h3>방: {roomTitle}</h3>
+
           <div className='lating_room'>
-            <div className='player'>
-              <div className='player_name'>
-                <div className='readydiv'>
-                  {props.ready == "false" ?
-                    <img></img> : <img src="img/ready.png"></img>}
-                </div>
-                <div className="player_infodiv">
-                  <span className='playername'>{playername}</span>
-                  <div className='exit'>
-                    <button className='exit_btn' onClick={function (e) { e.preventDefault(); alert(banish); }}>
-                      <img className="exit_img" src="img/delete_47751.png"></img>
-                      <img className="exit_img2" src="img/close_round_delete_remove_icon_177274.png"></img>
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div className="playerimg"><img src="img/ch1.png"></img></div>
-            </div>
 
-            <div className='player'>
-              <div className='player_name'>
-                <div className='readydiv'>
-                  {props.ready == "false" ?
-                    <img></img> : <img src="img/ready.png"></img>}
-                </div>
-                <div className="player_infodiv">
-                  <span className='playername'>플레이어2</span>
-                  
-                  <div className='exit'>
-                    <button className='exit_btn' onClick={function (e) { e.preventDefault(); alert("플레이어 2강퇴"); }}>
-                       <img className="exit_img" src="img/delete_47751.png"></img>
-                      <img className="exit_img2" src="img/close_round_delete_remove_icon_177274.png"></img>
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div className="playerimg"><img src="img/ch2.png"></img></div>
-            </div>
+          {/*player컴포넌트*/}
+          {players.map((player,index)=>(
+            <Player host={player.host} me={Myobject} ready={player.ready} playername={player.name}
+             playerImg={player.img_url} Playerobject={player} Eject={Eject}/>
+          ))}
 
-            <div className='player'>
-              <div className='player_name'>
-                <div className='readydiv'>
-                  {props.ready == "false" ?
-                    <img></img> : <img src="img/ready.png"></img>}
-                </div>
-                <div className="player_infodiv">
-                  <span className='playername'>플레이어3</span>             
-                  <div className='exit'>
-                    <button className='exit_btn' onClick={function (e) { e.preventDefault(); alert("플레이어 3강퇴"); }}>
-                     <img className="exit_img" src="img/delete_47751.png"></img>
-                      <img className="exit_img2" src="img/close_round_delete_remove_icon_177274.png"></img>
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div className="playerimg"><img src="img/ch3.png"></img></div>
-            </div>
-
-            <div className='player'>
-              <div className='player_name'>
-                <div className='readydiv'>
-                  {props.ready == "false" ?
-                    <img></img> : <img src="img/ready.png"></img>}
-                </div>
-                <div className="player_infodiv">
-                  <span className='playername'>플레이어4</span>               
-                  <div className='exit'>
-                    <button className='exit_btn' onClick={function (e) { e.preventDefault(); alert("플레이어 4강퇴"); }}>
-                      <img className="exit_img" src="img/delete_47751.png"></img>
-                      <img className="exit_img2" src="img/close_round_delete_remove_icon_177274.png"></img>
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div className="playerimg"><img src="img/ch4.png"></img></div>
-            </div>
-
-            <div className='player'>
-              <div className='player_name'>
-                <div className='readydiv'>
-                  {props.ready == "false" ?
-                    <img></img> : <img src="img/ready.png"></img>}
-                </div>
-                <div className="player_infodiv">
-                  <span className='playername'>플레이어2</span>                 
-                  <div className='exit'>
-                    <button className='exit_btn' onClick={function (e) { e.preventDefault(); alert("플레이어 5강퇴"); }}>
-                    <img className="exit_img" src="img/delete_47751.png"></img>
-                      <img className="exit_img2" src="img/close_round_delete_remove_icon_177274.png"></img>
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div className="playerimg"><img src="img/ch5.png"></img></div>
-            </div>
-
-             <div className='player'>
-              <div className='player_name'>
-                <div className='readydiv'>
-                  {props.ready == "false" ?
-                    <img></img> : <img src="img/ready.png"></img>}
-                </div>
-                <div className="player_infodiv">
-                  <span className='playername'>플레이어6</span>      
-                  <div className='exit'>
-                    <button className='exit_btn' onClick={function (e) { e.preventDefault(); alert("플레이어 6강퇴"); }}>
-                      <img className="exit_img" src="img/delete_47751.png"></img>
-                      <img className="exit_img2" src="img/close_round_delete_remove_icon_177274.png"></img>
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div className="playerimg"><img src="img/ch6.png"></img></div>
-            </div>
-
-            <div className='player'>
-              <div className='player_name'>
-                <div className='readydiv'>
-                  {props.ready == "false" ?
-                    <img></img> : <img src="img/ready.png"></img>}
-                </div>
-                <div className="player_infodiv">
-                  <span className='playername'>플레이어7</span>
-                  <div className='exit'>
-                    <button className='exit_btn' onClick={function (e) { e.preventDefault(); alert("플레이어 7강퇴"); }}>
-                     <img className="exit_img" src="img/delete_47751.png"></img>
-                      <img className="exit_img2" src="img/close_round_delete_remove_icon_177274.png"></img>
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div className="playerimg"><img src="img/ch7.png"></img></div>
-            </div>
-
-            <div className='player'>
-              <div className='player_name'>
-                <div className='readydiv'>
-                  {props.ready == "false" ?
-                    <img></img> : <img src="img/ready.png"></img>}
-                </div>
-                <div className="player_infodiv">
-                  <span className='playername'>플레이어8</span>       
-                  <div className='exit'>
-                    <button className='exit_btn' onClick={function (e) { e.preventDefault(); alert("플레이어 8강퇴"); }}>
-                      <img className="exit_img" src="img/delete_47751.png"></img>
-                      <img className="exit_img2" src="img/close_round_delete_remove_icon_177274.png"></img>
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div className="playerimg"><img src="img/ch8.png"></img></div>
-
-            </div>
           </div>
         </> : // 게임 시작하면 화면이 아래와 같이 바뀜
         <>
@@ -304,7 +200,8 @@ function GameSection(props) {
             <p>이용자의 ID가 담긴 문자열 배열 id_list, 각 이용자가 신고한 이용자의 ID 정보가 담긴 문자열 배열 report, 정지 기준이 되는 신고 횟수 k가 매개변수로 주어질 때, 각 유저별로 처리 결과 메일을 받은 횟수를 배열에 담아 return 하도록 solution 함수를 완성해주세요.</p>
           </div>
           <div className='inputsection'>
-            <textarea className="inputarea" value={codeInput} onKeyDown={codeInputTabHandler} onChange={codeInputHandler} />
+            <CodeSection style={{height: "600px",width: "100%"}} language={language} onChange={onChange}/>
+             {/* <textarea className="inputarea" value={codeInput} onKeyDown={codeInputTabHandler} onChange={codeInputHandler} /> */}
           </div>
           <button className='submit_btn' onClick={submit_text}>제출</button></>}
     </Box>
