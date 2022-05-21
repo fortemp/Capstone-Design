ea91671c667bed5c843ec92215408a4071fb1890
kimmingyu <kimmin36944@gmail.com>
2022-05-22 00:44:09 +0900

    변경: nav바 어드민페이지

--- client/src/components/Navbar/Sections/RightSide.js
+++ client/src/components/Navbar/Sections/RightSide.js
@@ -6,13 +6,14 @@ import { logoutUser } from '../../../actions/index'
 import LogoutIcon from '@mui/icons-material/Logout';
 import LoginIcon from '@mui/icons-material/Login';
 import BorderColorIcon from '@mui/icons-material/BorderColor';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import GiteIcon from '@mui/icons-material/Gite';
 
 function RightSide() {
 
     const dispatch = useDispatch();
-    const isAuth = useSelector(state => state.authReducer.authData.auth);// 사용자 정보 객체
-    const playername =useSelector(state => state.authReducer.authData.user.name)+'님 반갑습니다.';
+    const isAuth = useSelector(state => state.authReducer.authData.auth);
+    const playerobject = useSelector(state => state.authReducer.authData.user);// 사용자 정보 객체
     const logoutHandler = () => {
         dispatch(logoutUser())
             .then(res => {
@@ -27,10 +28,16 @@ function RightSide() {
 
     };
 
-    const [login, setlogin] = useState("false");
+    const [login, setlogin] = useState(false);
+    const [admin, setAdmin] = useState(false);
     useEffect(() => {          //로그인 확인 코드
-        if (isAuth === true)
-            setlogin("true");
+        if (isAuth === true){
+            setlogin(true);
+            if(playerobject.is_admin){
+                setAdmin(true)
+            }
+        }
+            
     })
 
     if (!isAuth) {
@@ -49,16 +56,18 @@ function RightSide() {
                         </textarea>
                     </div>
                     <div className='Info2'>
-                        <Button color="inherit"  onClick={function(){alert("로그인 후 이용해주세요")}}>
+                        <Button color="inherit">
+                            <a href="/community">
                                 <BorderColorIcon
                                     sx={{
                                         color: "#333333"
                                     }} />
-                                <span style={fontStyle}>COMMUNITY</span>                    
+                                <span style={fontStyle}>COMMUNITY</span>
+                            </a>
                         </Button>
 
                         <Button color="inherit" onClick={function(){alert("로그인 후 이용해주세요")}}>
-                                <BorderColorIcon
+                                <ShoppingCartIcon
                                     sx={{
                                         color: "#333333"
                                     }} />
@@ -89,6 +98,72 @@ function RightSide() {
             </div>
         )
     } else {
+        if(admin){
+            return (
+                <div>
+                <div id="logo" >
+                    <Button color="inherit" variant="h6" component="div">
+                        <a href="/">
+                            <span id="logotype">(C.C.C)</span>
+                        </a>
+                    </Button>
+                </div>
+                <div id="rightside">
+                    <textarea className='InfoMsg' resize='none' placeholder={`${playerobject.name}님 안녕하세요!`} disabled='false'>
+                    </textarea>
+                    <div className='Info2'>
+                        <Button color="inherit">
+                            <a href="/community">
+                                <BorderColorIcon
+                                    sx={{
+                                        color: "#333333"
+                                    }} />
+                                <span style={fontStyle}>COMMUNITY</span>
+                            </a>
+                        </Button>
+
+                        <Button color="inherit">
+                            <a href="/ProblemInsertpage">
+                                <BorderColorIcon
+                                    sx={{
+                                        color: "#333333"
+                                    }} />
+                                <span style={fontStyle}>problemInsert</span>
+                            </a>
+                        </Button>
+    
+                        <Button color="inherit">
+                            <a href="/shop">
+                                <ShoppingCartIcon
+                                    sx={{
+                                        color: "#333333"
+                                    }} />
+                                <span style={fontStyle}>SHOP</span>
+                            </a>
+                        </Button>
+                        <Button color="inherit">
+                            <a href="/host">
+                                <GiteIcon
+                                    sx={{
+                                        color: "#333333"
+                                    }} />
+                                <span style={fontStyle}>host</span>
+                            </a>
+                        </Button>
+                        <Button color="inherit">
+                            <a onClick={logoutHandler} href="/">
+                                <LogoutIcon
+                                    sx={{
+                                        color: "#333333"
+                                    }} />
+                                <span style={fontStyle}>logout</span>
+                            </a>
+                        </Button>
+                    </div>
+                </div>
+                </div>
+            )
+        }
         return (
             <div>
             <div id="logo" >
@@ -99,7 +174,7 @@ function RightSide() {
                 </Button>
             </div>
             <div id="rightside">
-                <textarea className='InfoMsg' resize='none' placeholder={playername} disabled='false'>
+                <textarea className='InfoMsg' resize='none' placeholder={`${playerobject.name}님 안녕하세요!`} disabled='false'>
                 </textarea>
                 <div className='Info2'>
                     <Button color="inherit">
@@ -114,7 +189,7 @@ function RightSide() {
 
                     <Button color="inherit">
                         <a href="/shop">
-                            <BorderColorIcon
+                            <ShoppingCartIcon
                                 sx={{
                                     color: "#333333"
                                 }} />
