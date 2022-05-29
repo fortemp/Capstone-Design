dabc594d2a0b5ea64063af8dd38cb9721e045bb5
abcd0978 <kimmin36944@gmail.com>
2022-05-30 04:17:45 +0900

    변경: grading 변경(callback방식으로)

--- server/util/GradingApi.js
+++ server/util/GradingApi.js
@@ -28,7 +28,7 @@ exports.MakeInputOutput = function(problemId, inputArr, outputArr){
         for(let i=1;i<=4;i++)
             fs.writeFileSync(`${dirPath}/in/${i}.in`,inputArr[i-1]);
         for(let i=1;i<=4;i++)
-            fs.writeFileSync(`${dirPath}/result/${i}.result`,outputArr[i-1].trim());
+            fs.writeFileSync(`${dirPath}/result/${i}.result`,outputArr[i-1]);
         return problemId
     }catch(err){
         console.log(err);
@@ -36,11 +36,14 @@ exports.MakeInputOutput = function(problemId, inputArr, outputArr){
     }
 }
 
-exports.executeCode = function(user_id,language,problemSetPath,CodeString,problemId,testLength,test){
+exports.executeCode = function(user_id,language,problemSetPath,CodeString,problemId,testLength,test,callback,res){
 
     var executeResult;//실행결과: 맞으면 1 그 이외는 0
     var executeLog;//실행결과에대한 로그, 맞음과 틀림에 상관없이 리턴한다.
     var message=''
+    const dirPath = path.join(__dirname,`../../grading/usercode/${user_id}`);
+    const binFilePath = path.join(dirPath,'../../src');
+
     function on_child_stdout(data) {//한줄한줄 분리해주는 함수
         let correctNum = 0;
         var str = data.toString();
@@ -68,34 +71,88 @@ exports.executeCode = function(user_id,language,problemSetPath,CodeString,proble
         }
     };
 
-    const dirPath = path.join(__dirname,`../../grading/usercode/${user_id}`);
-    const binFilePath = path.join(dirPath,'../../src');
-    let isExits = fs.existsSync(dirPath);
-    let codeId_random = RandomStringGenerator(10);
-    codeId_random = codeId_random.trim();
-    if(!isExits)//없으면 만듬
-        fs.mkdirSync(dirPath);
-    try{
-        fs.writeFileSync(`${dirPath}/${codeId_random}.${language}`,CodeString);
-        
-        const gpp = spawnSync('./a.out', [
-            '--user',user_id,
-            '--code-id',codeId_random,
-            '--problem-path',problemSetPath,
-            '--problem-id',problemId,
-            '--length',testLength,
-            '--language',language]
-            ,{cwd:binFilePath});
-        executeLog = fs.readFileSync(`${dirPath}/${codeId_random}.log`,'utf-8')
-        if(!on_child_stdout(gpp.stdout.toString())  &&  test){
-            //test문제이면서 실패하면 문제를 삭제해버린다.
-            fs.rmSync(path.join(__dirname,`../../grading/problem/${problemId}`),{recursive:true, force:true})
-            return {correct:executeResult}
+
+    fs.exists(dirPath,(exResult)=>{
+        let codeId_random = RandomStringGenerator(10);
+        codeId_random = codeId_random.trim();
+        if(!exResult){//없으면 만듬
+            fs.mkdir(dirPath,(e)=>{
+                fs.writeFile(`${dirPath}/${codeId_random}.${language}`,CodeString,(err)=>{
+                    const gpp = spawn('./a.out', [
+                        '--user',user_id,
+                        '--code-id',codeId_random,
+                        '--problem-path',problemSetPath,
+                        '--problem-id',problemId,
+                        '--length',testLength,
+                        '--language',language]
+                        ,{cwd:binFilePath});
+
+                     gpp.stdout.on('data', (data) => {
+                        fs.readFile(`${dirPath}/${codeId_random}.log`,'utf-8',(err,datal)=>{
+                            executeResult =  on_child_stdout(data.toString())
+                            executeLog = datal
+                            if(!executeResult){//테스트이면서 틀렸으면 그냥 문제 삭제해버리고 다시함
+                                if(test){
+                                    fs.rmSync(path.join(__dirname,`../../grading/problem/${problemId}`),{recursive:true, force:true})
+                                }
+                                callback({correct:executeResult, log:executeLog.toString(),message:message,problem_id:problemId,test:test,res:res})
+                            }else{
+                                callback({correct:executeResult, log:executeLog.toString(),message:message,problem_id:problemId,res:res})
+                            }
+                            
+                        })
+                    });
+                        
+                    gpp.stderr.on('data', (data) => {
+                    console.error(`stderr: ${data}`);
+                    });
+                    
+                    gpp.on('close', (code) => {
+                    console.log(`child process exited with code ${code}`);
+                    });
+
+                    
+                });
+                
+            });
+            
+        }else{//있으면 그냥 코드씀
+            fs.writeFile(`${dirPath}/${codeId_random}.${language}`,CodeString,(err)=>{
+                console.log('코드씀')
+                const gpp = spawn('./a.out', [
+                    '--user',user_id,
+                    '--code-id',codeId_random,
+                    '--problem-path',problemSetPath,
+                    '--problem-id',problemId,
+                    '--length',testLength,
+                    '--language',language]
+                    ,{cwd:binFilePath});
+
+                gpp.stdout.on('data', (data) => {
+                    executeResult =  on_child_stdout(data.toString())
+                    fs.readFile(`${dirPath}/${codeId_random}.log`,'utf-8',(err,datal)=>{
+                        executeResult =  on_child_stdout(data.toString())
+                        executeLog = datal
+                        if(!executeResult){//테스트이면서 틀렸으면 그냥 문제 삭제해버리고 다시함
+                            if(test){
+                                fs.rmSync(path.join(__dirname,`../../grading/problem/${problemId}`),{recursive:true, force:true})   
+                            }
+                            callback({correct:executeResult, log:executeLog.toString(),message:message,problem_id:problemId,test:test,res:res})
+                        }else{
+                            callback({correct:executeResult, log:executeLog.toString(),message:message,problem_id:problemId,res:res})
+                        }
+                    })
+                });
+                    
+                gpp.stderr.on('data', (data) => {
+                console.error(`stderr: ${data}`);
+                });
+                
+                gpp.on('close', (code) => {
+                console.log(`child process exited with code ${code}`);
+                });
+
+            });
         }
-        
-        //성공하면 문제번호와 함께 log, message를 보내준다.
-        return {correct:executeResult, log:executeLog.toString(),message:message,problem_id:problemId}
-    }catch(err){
-        console.log(err);
-    }
+    });
 }
